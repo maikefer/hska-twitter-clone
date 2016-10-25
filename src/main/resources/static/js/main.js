09052cabbf2c4f63180667b3fa7663ed0600a03f
Gennadi Eirich <gena.eirich@gmail.com>
2016-10-25 18:56:51 +0200

    Adds temporary user navigation

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,4 +2,76 @@
  * Created by bob on 19/10/2016.
  */
 
-console.log('main');
+$(function() {
+
+  var $timeline = $('#timeline');
+  var $followers = $('#followers');
+  var $profile = $('#profile-header');
+  var $followingTab = $('#following-tab');
+  var $followersTab = $('#followers-tab');
+
+  // Show followers
+  $('#btn-followers').on('click', function(ev) {
+    show('followers');
+
+    // Activate tab
+    activate('followers');
+  });
+
+  // Show whom im following
+  $('#btn-following').on('click', function(ev) {
+    show('followers');
+
+    // Activate tab
+    activate('following');
+  });
+
+  /**
+   * Show timeline
+   */
+  $profile.on('click', function(ev) {
+    var pathname = window.location.pathname;
+    // navigate to user
+    if (pathname === '/') {
+      window.location.href = "/user";
+    } else {
+      show('timeline');
+    }
+
+  });
+
+  /**
+   * Show a DOM container
+   * @param container
+   */
+  function show(container) {
+    switch (container) {
+      case 'followers':
+        $timeline.css('display', 'none');
+        $followers.css('display', 'block');
+        return;
+      case 'timeline':
+        $timeline.css('display', 'block');
+        $followers.css('display', 'none');
+        return;
+    }
+  }
+
+  /**
+   * Activates the desired tab
+   * @param tab
+   */
+  function activate(tab) {
+    switch (tab) {
+      case 'following':
+        $followersTab.removeClass('is-active');
+        $followingTab.addClass('is-active');
+        return;
+      case 'followers':
+        $followingTab.removeClass('is-active');
+        $followersTab.addClass('is-active');
+        return;
+    }
+  }
+
+});
