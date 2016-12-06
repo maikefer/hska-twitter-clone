5a1dcff77c07d6a9274572f1cc7c79065dda6d6d
Maike R <maikefer_@gmx.de>
2016-12-06 18:15:32 +0100

    Add input validation and error message style for registration

--- src/main/java/de/hska/lkit/registration/RegistrationViewController.java
+++ src/main/java/de/hska/lkit/registration/RegistrationViewController.java
@@ -26,37 +26,59 @@ public class RegistrationViewController {
     @Autowired
     private LoginController loginController;
 
-	@RequestMapping(value = "/registration", method = RequestMethod.GET)
-	public String showRegistrationView(Model model) {
-	    model.addAttribute("user", new User());
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String showRegistrationView(Model model) {
+        model.addAttribute("user", new User());
 
         return "registration";
-	}
+    }
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String showRegistrationView(@ModelAttribute("user") User userForm, HttpServletResponse response, Model model) {
+    public String showRegistrationView(@ModelAttribute("user") User userForm, HttpServletResponse response,
+                                       Model model) {
 
-	    if (userForm.getUsername() != null && userForm.getUsername().length() > 0)
-            if (userForm.getEmail() != null) // TODO: validate email
-                if (userForm.getPassword() != null && userForm.getPassword().length() > 0) {
-	                // Check if user already exists
-                    if (userRepo.isUsernameAvailable(userForm.getUsername())) {
-                        model.addAttribute("error", "This username is already taken. Try a new one");
-                        return "registration";
-                    }
+        if (!isValidUsername(userForm.getUsername())) {
+            model.addAttribute("error",
+                "Username is not valid. The Username can only contain Letters and Numbers.");
+            return "registration";
+        }
 
-                    // Save user
-                    userRepo.saveUser(userForm);
+        if (!isValidEmail(userForm.getEmail())) {  //todo validate mail
+            model.addAttribute("error", "Please type in a valid E-Mail Address");
+            return "registration";
+        }
+        if (!isValidPassword(userForm.getPassword())) {
+            model.addAttribute("error", "Please type in a correct Password");
+            return "registration";
+        }
+        if (userRepo.isUsernameAvailable(userForm.getUsername())) {
+            model.addAttribute("error", "This username is already taken. Try a new one");
+            return "registration";
+        }
 
-                    // Log user in
-                    loginController.login(userForm, response);
+        userRepo.saveUser(userForm);
+        loginController.login(userForm, response);
+        return "redirect:/"; // redirect to home page
+    }
 
-                    // redirect to home page
-                    return "redirect:/";
-                }
+    private boolean isValidEmail(String email) {
+        return email != null && email.length() > 0
+            && email.matches("[a-zA-Z0-9-_.]+[@][a-zA-Z0-9-_.]+[.][a-zA-Z0-9-_.]+") ;
+    }
 
-        model.addAttribute("error", "Provided data not valid");
-        return "registration";
+    private boolean isValidPassword(String password) {
+        return password != null && password.length() > 0 && !password.equals(" ");
+    }
+
+    /**
+     * The Username is only allowed to contain a-z, A-Z and 0-9.
+     * No special characters, no ä, ü, ö
+     *
+     * @param username The string that will be checked
+     * @return If the sting is a valid username
+     */
+    private boolean isValidUsername(String username) {
+        return username != null && username.length() > 0 && username.matches("[a-zA-Z0-9]+");
     }
 
 }
