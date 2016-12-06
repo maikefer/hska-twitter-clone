af730dc29cbbd7f7a36afdbbe07e19c3ac3b4abb
daB0bby <gena.eirich@gmail.com>
2016-12-06 20:00:58 +0100

    Fixes typo

--- src/test/java/de/hska/lkit/redis/repo/UserRepositoryIT.java
+++ src/test/java/de/hska/lkit/redis/repo/UserRepositoryIT.java
@@ -16,7 +16,7 @@ import de.hska.lkit.redis.model.User;
 public class UserRepositoryIT {
 
 	@Autowired
-	private UserRepository userRepositroy;
+	private UserRepository userRepository;
 
 	private User user;
 
@@ -31,27 +31,27 @@ public class UserRepositoryIT {
 
 	@Test
 	public void basic() {
-		assertNotNull(this.userRepositroy);
+		assertNotNull(this.userRepository);
 	}
 
 	@Test
 	public void createUser() {
-		assertEquals("Username should be still available", true, this.userRepositroy.isUsernameAvailable(this.user.getUsername()) );
+		assertEquals("Username should be still available", true, this.userRepository.isUsernameAvailable(this.user.getUsername()) );
 
-		int numUsersBefore = this.userRepositroy.findAllUsers().size();
-		this.userRepositroy.saveUser(this.user);
-		int numUsersAfter = this.userRepositroy.findAllUsers().size();
+		int numUsersBefore = this.userRepository.findAllUsers().size();
+		this.userRepository.saveUser(this.user);
+		int numUsersAfter = this.userRepository.findAllUsers().size();
 
 		assertEquals("Number of users has increased", 1, numUsersAfter - numUsersBefore);
 
-		assertEquals("Username shouldn't be available anymore", false, this.userRepositroy.isUsernameAvailable(this.user.getUsername()) );
+		assertEquals("Username shouldn't be available anymore", false, this.userRepository.isUsernameAvailable(this.user.getUsername()) );
 	}
 
 	@Test
 	public void findUser() {
 		createUser();
 
-		User foundUser = this.userRepositroy.findUser( user.getUsername() );
+		User foundUser = this.userRepository.findUser( user.getUsername() );
 
 		assertEquals("The found user should be equal to the test user", this.user, foundUser);
 
@@ -60,9 +60,9 @@ public class UserRepositoryIT {
 
 	@Test
 	public void deleteUser() {
-		int numUsersBefore = this.userRepositroy.findAllUsers().size();
-		this.userRepositroy.deleteUser(this.user);
-		int numUsersAfter = this.userRepositroy.findAllUsers().size();
+		int numUsersBefore = this.userRepository.findAllUsers().size();
+		this.userRepository.deleteUser(this.user);
+		int numUsersAfter = this.userRepository.findAllUsers().size();
 
 		assertEquals("Number of users has decreased", -1, numUsersAfter - numUsersBefore);
 	}
@@ -72,13 +72,13 @@ public class UserRepositoryIT {
 		String user1 = "test:user1";
 		String user2 = "test:user2";
 
-		this.userRepositroy.startFollowUser(user1, user2);
-		assertTrue("User2 is a follower of User1",  this.userRepositroy.findFollowers(user1).contains(user2));
-		assertTrue("User2 is following User1",  this.userRepositroy.findFollowing(user2).contains(user1));
+		this.userRepository.startFollowUser(user1, user2);
+		assertTrue("User2 is a follower of User1",  this.userRepository.findFollowers(user1).contains(user2));
+		assertTrue("User2 is following User1",  this.userRepository.findFollowing(user2).contains(user1));
 
-		this.userRepositroy.stopFollowUser(user1, user2);
-		assertFalse("User2 is not a follower of User1 anymore",  this.userRepositroy.findFollowers(user1).contains(user2));
-		assertFalse("User2 is not following User1 anymore",  this.userRepositroy.findFollowing(user2).contains(user1));
+		this.userRepository.stopFollowUser(user1, user2);
+		assertFalse("User2 is not a follower of User1 anymore",  this.userRepository.findFollowers(user1).contains(user2));
+		assertFalse("User2 is not following User1 anymore",  this.userRepository.findFollowing(user2).contains(user1));
 	}
 
 }
