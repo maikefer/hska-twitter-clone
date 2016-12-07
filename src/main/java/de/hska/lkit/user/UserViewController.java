d533dd2d662256749417646e51993b0c0045925b
daB0bby <gena.eirich@gmail.com>
2016-12-07 22:09:09 +0100

    Folowing/followers works properly now
    Fixes #9

--- src/main/java/de/hska/lkit/user/UserViewController.java
+++ src/main/java/de/hska/lkit/user/UserViewController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -21,7 +20,7 @@ import java.util.Set;
 
 
 @Controller
-public class UserViewController {
+public class  UserViewController {
 
     @Autowired
     private UserRepository userRepository;
@@ -64,8 +63,8 @@ public class UserViewController {
         return "user";
     }
 
-    @RequestMapping(value = "/follow/{username}")
-    public String followUser(@PathVariable("username") String username, HttpServletRequest request, Model model) {
+    @RequestMapping(value = { "/follow/{username}", "/follow/{username}/{user}/{tab}" })
+    public String followUser(@PathVariable("username") String username, @PathVariable(value = "user", required = false) String user, @PathVariable(value = "tab", required = false) String tab) {
 
         // Get users
         User currentUser = userRepository.findUser(SessionSecurity.getName());
@@ -73,19 +72,14 @@ public class UserViewController {
 
         userRepository.startFollowUser(currentUser.getUsername(), userToFollow.getUsername());
 
-        if (request.getHeader("referer").contains("follower")) {
-            int index = request.getHeader("referer").indexOf("8080");
-            String referrer = request.getHeader("referer").substring(index + 5);
-
-            // Do redirect to same page
-            return "redirect:/" + referrer;
-        }
+        if (tab != null && user != null)
+            return "redirect:/users/" + user + "/" + tab;
 
         return "redirect:/users/" + username;
     }
 
-    @RequestMapping(value = "/unfollow/{username}")
-    public String unFollowUser(@PathVariable("username") String username, HttpServletRequest request, Model model) {
+    @RequestMapping(value = { "/unfollow/{username}", "/unfollow/{username}/{user}/{tab}" })
+    public String unFollowUser(@PathVariable("username") String username, @PathVariable(value = "user", required = false) String user, @PathVariable(value = "tab", required = false) String tab) {
 
         // Get users
         User currentUser = userRepository.findUser(SessionSecurity.getName());
@@ -93,13 +87,8 @@ public class UserViewController {
 
         userRepository.stopFollowUser(currentUser.getUsername(), userToUnFollow.getUsername());
 
-        if (request.getHeader("referer").contains("follower")) {
-            int index = request.getHeader("referer").indexOf("8080");
-            String referrer = request.getHeader("referer").substring(index + 5);
-
-            // Do redirect to same page
-            return "redirect:/" + referrer;
-        }
+        if (tab != null && user != null)
+            return "redirect:/users/" + user + "/" + tab;
 
         return "redirect:/users/" + username;
     }
@@ -141,17 +130,67 @@ public class UserViewController {
         // Following list
         List<Follower> listFollowing = new ArrayList<>();
         for (String name : follower)
-            listFollowing.add(new Follower(name, this.following(user.getUsername(), name)));
+            listFollowing.add(new Follower(name, this.following(currentUser.getUsername(), name)));
         model.addAttribute("listFollowing", listFollowing);
 
         // Follower list
         List<Follower> listFollower = new ArrayList<>();
         for (String name : following)
-            listFollower.add(new Follower(name, this.following(user.getUsername(), name)));
+            listFollower.add(new Follower(name, this.following(currentUser.getUsername(), name)));
         model.addAttribute("listFollower", listFollower);
 
         model.addAttribute("isSelf", SessionSecurity.getName().equals(user.getUsername()));
         return "follower";
+    }
+
+    @SuppressWarnings("Duplicates")
+    @RequestMapping(value = "/users/{username}/following")
+    public String showFollowing(@PathVariable("username") String username, Model model) {
+
+        // Get currentUser
+        User currentUser = userRepository.findUser(SessionSecurity.getName());
+        model.addAttribute("currentUser", currentUser);
+
+        // Get requested user
+        User user = userRepository.findUser(username);
+        if (user != null) {
+            model.addAttribute("user", user);
+        } else {
+            // User not found
+            String errorMessage = "User '" + username + "' not found!";
+            model.addAttribute("errorMessage", errorMessage);
+            return "error";
+        }
+
+        if (!SessionSecurity.getName().equals(user.getUsername())) { // Another user
+            boolean isFollowing = this.following(currentUser.getUsername(), user.getUsername());
+            model.addAttribute("isFollowing", isFollowing);
+        } else {
+            model.addAttribute("isFollowing", false);
+        }
+
+        // Follower
+        Set<String> follower = userRepository.findFollowers(user.getUsername());
+        Set<String> following = userRepository.findFollowing(user.getUsername());
+
+        // Follower Count
+        model.addAttribute("followingCnt", follower.size());
+        model.addAttribute("followerCnt", following.size());
+
+        // Following list
+        List<Follower> listFollowing = new ArrayList<>();
+        for (String name : follower)
+            listFollowing.add(new Follower(name, this.following(currentUser.getUsername(), name)));
+        model.addAttribute("listFollowing", listFollowing);
+
+        // Follower list
+        List<Follower> listFollower = new ArrayList<>();
+        for (String name : following)
+            listFollower.add(new Follower(name, this.following(currentUser.getUsername(), name)));
+        model.addAttribute("listFollower", listFollower);
+
+        model.addAttribute("isSelf", SessionSecurity.getName().equals(user.getUsername()));
+        return "following";
     }
 
     /**
