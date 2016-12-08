78d1061166ed8922946a7574ab73c9b7cbdbffda
daB0bby <gena.eirich@gmail.com>
2016-12-08 14:56:10 +0100

    Some improvements

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,6 +1,13 @@
 /**
  * Created by bob on 20/10/2016.
  */
-document.getElementById('postText').onkeyup = function () {
-  document.getElementById('counter').innerHTML = (140 - this.value.length);
-};
+
+$(function() {
+
+  document.getElementById('postText').onkeyup = function () {
+    document.getElementById('counter').innerHTML = (140 - this.value.length);
+  };
+
+});
+
+
