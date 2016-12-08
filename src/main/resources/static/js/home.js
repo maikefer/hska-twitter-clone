abd7fc1c8bf7033b9bf10ccd45ad35d97b5f98fc
Maike R <maikefer_@gmx.de>
2016-12-08 13:36:00 +0100

    Add dynamic counter for text input field

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,3 +1,6 @@
 /**
  * Created by bob on 20/10/2016.
  */
+document.getElementById('postText').onkeyup = function () {
+  document.getElementById('counter').innerHTML = (140 - this.value.length);
+};
